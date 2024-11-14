a6499a4ccd3080f8e156359efda9cb0ddd13bda9
Suraj <108755893+surajgajare123@users.noreply.github.com>
2024-11-14 13:36:49 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main{
+    public static void main(String[] args){
+        // Introduced java loops
+        // while loop 
+        int a = 0;
+        while(a<=12){
+            System.out.println("hello coder!");
+            a++;
+        }
+        // do while loop
+        int b = 0;
+        do{
+            System.out.println("hello do while loop!");
+            b++;
+        }while(b<5);
+        // for loop
+        for(int i = 0; i < 6; i++){
+            System.out.println("hello here is for loop!");
+        }
+    }
+}
